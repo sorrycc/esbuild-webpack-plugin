44bdecbc2bbc23b83ae21bd7d882e637756f908f
chencheng <sorrycc@gmail.com>
2020-05-06 22:04:35 +0800

    chore: update publish script

--- src/umiPlugin.ts
+++ src/umiPlugin.ts
@@ -1,7 +1,7 @@
 import { IApi } from 'umi';
 
 export default (api: IApi) => {
-  api.modifyBundleConfig(memo => {
+  api.modifyBundleConfig((memo) => {
     if (memo.optimization) {
       memo.optimization.minimizer = [new (require('./index').default)()];
     }
